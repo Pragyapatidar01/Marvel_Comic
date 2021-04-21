65bfd8cce409e433adf7498acdd6225c53c62fec
Pragya <62202536+Pragyapatidar01@users.noreply.github.com>
2021-04-21 18:19:08 +0530

    Update CharacterUrl.js

--- src/CharacterUrl.js
+++ src/CharacterUrl.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CharacterUrl = ({itemUrl}) => {
     return (
-        <div className="single-url">
+        <div meta="content-security-policy" className="single-url">
             <a href={itemUrl.url} className="mylinks" rel="noreferrer" target="_blank">{itemUrl.type}s</a>
         </div>
     )
